268cf23140463721a170bcffd5ff5688ee5de1d9
livinginthesunlight72@protonmail.com <livinginthesunlight72@protonmail.com>
2023-04-08 22:01:02 -0500

    Inicio

---
+++ main.js
@@ -0,0 +1,44 @@
+const $ = s => document.querySelector(s);
+ 
+ const $editor = $('textarea')
+ const $view = $('iframe')
+
+ function update(){
+  $view.srcdoc = editor.getValue()
+ }
+
+
+ editor = ace.edit($editor);
+ editor.renderer.setPadding(20)
+ editor.getSession().on("change", update);
+ 
+editor.on("changeSelection", function(){
+  setTimeout(() => editor.centerSelection(), 0)
+});
+ editor.setOptions({
+  showGutter: false,
+  animatedScroll: false,
+  scrollPastEnd: 1,
+  fontSize: "17px",
+  //fontFamily: 'Victor',
+  dragEnabled: false, //nosirve
+  wrap: true, //imposible redline
+  printMargin: false,
+  tabSize: 1,
+  enableBasicAutocompletion: true,
+  enableLiveAutocompletion: true,
+  mode: "ace/mode/html",
+  theme:"ace/theme/monokai"
+ })
+
+
+window.onresize = function(){
+  setTimeout(() => editor.centerSelection(), 0)
+}
+  
+
+
+
+
+
+
